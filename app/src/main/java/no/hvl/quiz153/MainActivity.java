07feaf57b8ba1534885060024dca43a637c305aa
582637 <582637@stud.hvl.no>
2023-02-06 13:51:39 +0100

    Initial commit

---
+++ app/src/main/java/no/hvl/quiz153/MainActivity.java
@@ -0,0 +1,27 @@
+package no.hvl.quiz153;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonEasy = (Button) findViewById(R.id.button_easy);
+        buttonEasy.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              Log.d("tag", "AAAAAAAAA");
+                                          }
+                                      }
+
+        );
+    }
+}
